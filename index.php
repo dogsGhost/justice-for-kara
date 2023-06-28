2ea427dbf94f5521f669ef9e84675c48e7f2c2f0
David Wilhelm <davwilh@gmail.com>
2023-06-28 15:45:25 -0400

    Update index.php
    update analytics

--- index.php
+++ index.php
@@ -79,15 +79,14 @@ $admin = true;
   </div>
 </div>
 <script src="js/bundle.js"></script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-EB4WJEFK8H"></script>
 <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-  ga('create', 'UA-61911549-1', 'auto');
-  ga('send', 'pageview');
-
+  gtag('config', 'G-EB4WJEFK8H');
 </script>
 </body>
 </html>
